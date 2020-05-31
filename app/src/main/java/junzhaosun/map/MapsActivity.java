2aff1e0f8c90b2ad7fb21463d081246b4bce70c0
Junzhao Sun <jsun13@email.wm.edu>
2020-05-30 21:44:10 -0400

    redirect to new address upon click saved places

--- app/src/main/java/junzhaosun/map/MapsActivity.java
+++ app/src/main/java/junzhaosun/map/MapsActivity.java
@@ -21,12 +21,14 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import android.widget.Toast;
@@ -43,6 +45,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -57,9 +60,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Constants constant=new Constants();
     private List<LatLng> listLocation;
     private Marker curmark;
-    private boolean hasSearch=false;
-
     private SharedPreferences sharedPreferences;
+    private Location myLocation;
+    private Marker searchMark;
+//    private boolean hasSearch=false;
     /***
      * Whether a user responses yes or no to location request
      * @param requestCode
@@ -93,117 +97,114 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        TextInputEditText inputEditText=(TextInputEditText) findViewById(R.id.inputLoc);
+        final TextInputEditText inputEditText=(TextInputEditText) findViewById(R.id.inputLoc);
+
+
         inputEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if(hasFocus){
-                    Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-                    startActivity(intent);
+                    startActivityForResult(new Intent(getApplicationContext(), SearchActivity.class), 1);
+                    inputEditText.clearFocus();
                 }
             }
         });
         inputEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-                startActivity(intent);
+                startActivityForResult(new Intent(getApplicationContext(), SearchActivity.class), 1);
             }
         });
 
+
+        Button Brelocate=(Button) findViewById(R.id.relocate);
+        Brelocate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                centerMapOnLocation(myLocation, "my Location");
+            }
+        });
+
+        Intent intent=getIntent();
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query=intent.getStringExtra(SearchManager.QUERY);
+            Log.e("query", "get query from search!");
+            Toast.makeText(getApplicationContext(), "receive query "+query, Toast.LENGTH_SHORT).show();
+            if(SearchActivity.queries!=null && query!=null){
+                SearchActivity.queries.add(query);
+                moveToQuery(query);
+            }
+        }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String searchLoc=data.getStringExtra("query");
+                Toast.makeText(getApplicationContext(), "receive result"+searchLoc, Toast.LENGTH_SHORT).show();
+                moveToQuery(searchLoc);
+            }
+        }
+    }
+
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         addMarker();
-
-        Intent intent=getIntent();
-//
-//        String query=null;
-//        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-//            query=intent.getStringExtra(SearchManager.QUERY);
-//            Log.e("query", "get query from search!");
-////            Toast.makeText(getApplicationContext(), "get query from search!"+query, Toast.LENGTH_SHORT).show();
-//        }
-
-        if(!intent.hasExtra("query")){
-            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-            locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    if(isFirstTime){
-                        centerMapOnLocation(location, "Your location");
-                        Toast.makeText(getApplicationContext(),"enter first", Toast.LENGTH_SHORT).show();
-                        isFirstTime=false;
-                    }else if(!isFirstTime && !hasSearch){
-                        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                        if(curmark!=null){
-                            curmark.remove();
-                        }
-                        curmark=mMap.addMarker(new MarkerOptions().position(userLocation).title("Your location"));
-                    }
-
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String provider) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String provider) {
-
-                }
-            };
-
-            /**
-             * Check whether location is allowed and updates location
-             */
-            if(Build.VERSION.SDK_INT < 23){
-                Log.i("You need to update", "update your version");
-            }else{
-                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                }else{
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-//                    Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//                    centerMapOnLocation(lastKnownLocation, "Your Location");
-//                    Toast.makeText(getApplicationContext(),"enter validation", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-        }else{
-
-            String query=intent.getStringExtra("query");
-            Geocoder geocoder=new Geocoder(this);
-            try{
-
-                List<Address> addresses=geocoder.getFromLocationName(query, 1);
-                if(addresses != null && addresses.size()>0) {
-                    Address address=addresses.get(0);
-                    LatLng newloc=new LatLng(address.getLatitude(),address.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(newloc).title("new location"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newloc, 14));
-                }
-
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-
-
+        setUpMap();
     }
 
+    private void setUpMap(){
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if(isFirstTime){
+                    centerMapOnLocation(location, "Your location");
+                    Toast.makeText(getApplicationContext(),"enter first", Toast.LENGTH_SHORT).show();
+                    isFirstTime=false;
+                }else if(!isFirstTime){
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    if(curmark!=null){
+                        curmark.remove();
+                    }
+                    curmark=mMap.addMarker(new MarkerOptions().position(userLocation).title("Your location"));
+                }
+                myLocation=location;
+
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        /**
+         * Check whether location is allowed and updates location
+         */
+        if(Build.VERSION.SDK_INT < 23){
+            Log.i("You need to update", "update your version");
+        }else{
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
 
     private void addMarker(){
         sharedPreferences= getSharedPreferences("data", Context .MODE_PRIVATE);
@@ -229,8 +230,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
             if(curmark!=null) curmark.remove();
-            curmark=mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 14));
+            if(mMap!=null){
+                curmark=mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 14));
+            }
             Toast.makeText(getApplicationContext(),title, Toast.LENGTH_SHORT).show();
         }
     }
@@ -253,4 +256,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    private void moveToQuery( String query){
+        Geocoder geocoder=new Geocoder(this);
+        try{
+
+            List<Address> addresses=geocoder.getFromLocationName(query, 1);
+            if(addresses != null && addresses.size()>0) {
+                Address address=addresses.get(0);
+                LatLng newloc=new LatLng(address.getLatitude(),address.getLongitude());
+                if(searchMark!=null) searchMark.remove();
+                if(mMap!=null){
+                    searchMark=mMap.addMarker(new MarkerOptions().position(newloc).title("new location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newloc, 14));
+                }
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
